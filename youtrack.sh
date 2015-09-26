6a23ec36a61a6f3a453befe7b64f8cce12cf6da0
Daniel Zwicker <email@daniel-zwicker.de>
2015-09-26 23:07:16 +0200

    fix a error with too little random bits

--- youtrack.sh
+++ youtrack.sh
@@ -3,6 +3,7 @@
 exec java \
 	-XX:MaxMetaspaceSize=250m -Xmx1g \
 	-Duser.home=/var/lib/youtrack \
+	-Djava.security.egd=/dev/zrandom \
 	-Djavax.net.ssl.trustStore=/etc/ssl/certs/java/cacerts -Djavax.net.ssl.trustStorePassword=changeit \
 	-Djetbrains.youtrack.disableBrowser=true -Djetbrains.youtrack.enableGuest=false \
 	-Djetbrains.mps.webr.log4jPath=/etc/youtrack/log4j.xml -Djava.awt.headless=true \
